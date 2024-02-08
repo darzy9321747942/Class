fb88b4ad458defc3db2054b737a40929c362fac8
Pratham Darji <darjipratham53@gmail.com>
2024-02-08 19:20:57 +0530

    Add files via upload

---
+++ 35 class.dart
@@ -0,0 +1,13 @@
+void main() {
+ int value=Students().add(20,100);
+  print(value);
+}
+class Students
+{
+  int add(int num1,int num2)
+  {
+   int number=num1+num2;return number;
+    
+//     print(number);
+  }
+}
